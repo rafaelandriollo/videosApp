95718c38756753b2416ba0fbedb2ef6b93ebb278
Rafael de Carvalho Andriollo <rafael.andriollo@fatec.sp.gov.br>
2021-05-26 02:23:12 -0300

    Adicionamos o array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -10,6 +11,26 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Um Lugar Silencioso (2018)',
+      lancamento: '05/04/2018 (BR)',
+      duracao: '1h 30min',
+      classificacao: 76,
+      cartaz: 'https://m.media-amazon.com/images/M/MV5BMjI0MDMzNTQ0M15BMl5BanBnXkFtZTgwMTM5NzM3NDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      generos: ['Drama', 'Horror', 'Sci-Fi']
+    },
+    {
+      nome: 'A Bruxa (2015)',
+      lancamento: '03/03/2016 (BR)',
+      duracao: '1h 32min',
+      classificacao: 69,
+      cartaz: 'https://m.media-amazon.com/images/M/MV5BMTUyNzkwMzAxOF5BMl5BanBnXkFtZTgwMzc1OTk1NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      generos: ['Drama', 'Horror', 'Mistery']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
